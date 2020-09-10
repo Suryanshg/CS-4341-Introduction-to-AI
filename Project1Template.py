e46302e3832a975b824f6761b7db790d97b55620
Suryanshg <Suryanshg47@gmail.com>
2020-09-10 16:42:35 -0400

    Added new template

--- Project1Template.py
+++ Project1Template.py
@@ -3,109 +3,47 @@ import Graph
 import sys
 from collections import OrderedDict
 # Template for Project 1 of CS 4341 - A2020
-def General_Search(problem : Graph.Graph, search : SearchEnum):
+def General_Search(problem, searchMethod):
     """
-    Return the solution path or failure to reach node G from node S. Also outputs the expanded nodes and the queues before each expansion.
+    Return the solution path or failure to reach state G from state S. 
     
     Parameters
     ----------
     problem : Graph.Graph
         The graph to search from S to G.
-    search : SearchEnum.SearchEnum
+    searchMethod : SearchEnum
         The search method to use to search the graph.
     """
     initialState = 'S' # name of the initial state
     finalState = 'G' # Name of the final state
-    # Make_Queue, Make_Node, Remove_Front, State[node], Expand, and expand_queue are to be implemented by the student. Implementation may vary depending on the data structures used.
-    queue = Make_Queue(Make_Node(problem.getState(initialState)))
-    while len(queue) > 0:
-        node = Remove_Front(queue)
-        if State[node] is solution:
-            return State[node]
-        opened_nodes = Expand(node)
-        expand_queue(queue,opened_nodes,problem,search)
-    print("failure to find path between S and G")
+
+    # Make_Queue, Make_Queue_Node, Remove_Front, Terminal_State, Expand, and expand_queue are to be implemented by the student. 
+    # Implementation of the below pseudocode may vary slightly depending on the data structures used.
+
+    # queue = Make_Queue(Make_Queue_Node(problem.getState(initialState))) # Initialize the data structures to start the search at initialState
+    # while len(queue) > 0:
+    #     node = Remove_Front(queue) # Remove and return the node to expand from the queue
+    #     if Terminal_State(node) is solution: # solution is not a defined variable, but this statement represents checking whether you have expanded the goal node.
+    #         return node # If this is a solution, return the node containing the path to reach it.
+    #     opened_nodes = Expand(node) # Get new nodes to add to the queue based on the expanded node.
+    #     expand_queue(queue,opened_nodes,problem,searchMethod)
     return False
-
-def Make_Queue(node):
+#def expand_queue(queue, nodesToAddToQueue, problem, searchMethod):
     """
-    Returns a queue with the path inserted
-
-    Parameters
-    ----------
-    node : Graph.Node
-        Represents a node in the graph.
-
-    """
-    queue=[]
-    
-    return queue
-
-def Make_Node(node):
-    """
-    
-
-    Parameters
-    ----------
-    node : Graph.Node
-        Represents a node from the Graph
-
-    Returns
-    -------
-    A Node of type Graph.Node
-
-    """
-    
-    return
-
-def Remove_Front(queue):
-    """
-
-    Parameters
-    ----------
-    queue : list
-       Represents the queue of paths.
-
-    Returns 
-    -------
-    the removed node from the front of the queue
-
-    """
-    return
-
-def Expand(node):
-    """
-    
-
-    Parameters
-    ----------
-    node : Graph.Node
-        Represents a node in the Graph
-
-    Returns
-    -------
-    a list of paths to add to the queue.
-
-    """
-    
-    return
-   
-def expand_queue(queue, pathsToAddToQueue, problem, search):
-    """
-    Add the new paths created from the opened nodes to the queue based on the search strategy.
+    Add the new nodes created from the opened nodes to the queue based on the search strategy.
 
     Parameters
     ----------
     queue 
-        The queue containing the possible paths to expand upon for the search.
-    newPathsToAddToQueue : list
-        The list of paths to add to the queue.
+        The queue containing the possible nodes to expand upon for the search.
+    newNodesToAddToQueue : list
+        The list of nodes to add to the queue.
     problem : Graph.Graph
         The graph to search from S to G.
-    search : SearchEnum.SearchEnum
+    searchMethod : SearchEnum
         The search method to use to search the graph.
     """
-    #Fill in the below if and elif bodies to implement how the respective searches add new paths to the queue.
+    #Fill in the below if and elif bodies to implement how the respective searches add new nodes to the queue.
     # if search == SearchEnum.DEPTH_FIRST_SEARCH:
 
     # elif search == SearchEnum.BREADTH_FIRST_SEARCH:
@@ -124,7 +62,7 @@ def expand_queue(queue, pathsToAddToQueue, problem, search):
 
     # elif search == SearchEnum.BEAM_SEARCH:
 
-def main(filename : str):
+def main(filename):
     """
     Entry point for this program. Parses the input and then runs each search on the parsed graph.
 
@@ -141,8 +79,9 @@ def main(filename : str):
             print("failure to find path between S and G")
         else:
             print("\tgoal reached!")
+            # Print solution path here
         print()
-def readInput(filename : str) -> Graph.Graph:
+def readInput(filename):
     """
     Build the graph from the given input file.
 
@@ -163,16 +102,16 @@ def readInput(filename : str) -> Graph.Graph:
                 if(sectionDivider in line):
                     isHeuristicSection = True
                 elif(isHeuristicSection):
-                    node, heurStr = line.split(' ')
+                    state, heurStr = line.split(' ')
                     heuristic = float(heurStr)
-                    parsedGraph.setHeuristic(node, heuristic)
+                    parsedGraph.setHeuristic(state, heuristic)
                 else:
-                    node1, node2, costStr = line.split(' ')
+                    state1, state2, costStr = line.split(' ')
                     cost = float(costStr)
-                    parsedGraph.addNodesAndEdge(node1,node2, cost)
-    for node_key in parsedGraph.nodes:
-        node = parsedGraph.nodes[node_key]
-        node.edges = OrderedDict(sorted(node.edges.items()))
+                    parsedGraph.addStatesAndEdge(state1,state2, cost)
+    for state_key in parsedGraph.states:
+        state = parsedGraph.states[state_key]
+        state.edges = OrderedDict(sorted(state.edges.items()))
     return parsedGraph   
 if __name__ == "__main__": 
     if len(sys.argv) == 2:
