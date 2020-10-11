efd83beda9399d4bee813e50b92266b2c3d18b1e
Suryanshg <Suryanshg47@gmail.com>
2020-10-11 16:26:32 -0400

    Added code for preprocessing. Need to do random stratified sampling

--- Project3/template_proj3.py
+++ Project3/template_proj3.py
@@ -1,6 +1,37 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from keras.utils import to_categorical
 import numpy as np
+
+# Preprocessing data
+imgData = np.load('images.npy')
+labelData = np.load('labels.npy')
+
+flatImgData=[] # Flattened image data from 28X28 matrix to a vector of 784 values
+for i in range(len(imgData)):
+    flatImgData.append(imgData[i].flatten())
+
+oneHotVectors = to_categorical(labelData)
+
+data = [] # List of dataSets of image data, indexed by their truth values for labels.
+for i in range(10):
+    data.append([])  
+
+for i in range(len(flatImgData)):
+    index = np.argmax(oneHotVectors[i])
+    data[index].append(flatImgData[i])
+
+
+
+
+
+
+
+    
+
+
+    
+
 
 # Model Template
 
@@ -23,13 +54,10 @@ model.compile(optimizer='sgd',
               metrics=['accuracy'])
 
 # Train Model
-history = model.fit(x_train, y_train, 
-                    validation_data = (x_val, y_val), 
-                    epochs=10, 
-                    batch_size=512)
+#history = model.fit(x_train, y_train,  validation_data = (x_val, y_val), epochs=10, batch_size=512)
 
 
 # Report Results
 
-print(history.history)
-model.predict()
+#print(history.history)
+#model.predict()
