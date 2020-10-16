4a977d86b9f194aca1bb36acb83c6f5004c4c84f
Suryanshg <Suryanshg47@gmail.com>
2020-10-16 04:43:30 -0400

    Saving best model

--- Project3/project3.py
+++ Project3/project3.py
@@ -2,6 +2,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras.utils import to_categorical
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Preprocessing data
 imgData = np.load('images.npy')
@@ -29,16 +30,16 @@ val_data = []
 test_data = []
 
 for data in dataSet: # Doing Stratified Sampling
+    np.random.shuffle(data) # Performing Random Shuffling
     upperLimit60 = int(len(data) * 0.6)
     upperLimit75 = int(len(data) * 0.75)
-    train_data.extend(data[0:upperLimit60+1])
-    val_data.extend(data[upperLimit60+1:upperLimit75+1])
-    test_data.extend(data[upperLimit75+1:])
+    train_data.extend(data[0:upperLimit60])
+    val_data.extend(data[upperLimit60:upperLimit75])
+    test_data.extend(data[upperLimit75:])
 
-# Performing Random Shuffling
-np.random.shuffle(train_data)
-np.random.shuffle(test_data)
-np.random.shuffle(val_data)
+# np.random.shuffle(train_data)
+# np.random.shuffle(test_data)
+# np.random.shuffle(val_data)
 
 # Segregating Concatenated data into respective x and y data
 x_train = []
@@ -64,12 +65,17 @@ for data in test_data:
 
 
 # Model Template
-
 model = Sequential() # declare model
-model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
-model.add(Activation('sigmoid'))
+model.add(Dense(100, input_shape=(28*28, ), kernel_initializer='random_normal')) # first layer
+model.add(Activation('relu'))
 
-# model.add(Dense(2, kernel_initializer='he_normal')) # second layer
+model.add(Dense(200, kernel_initializer='random_normal')) # Second layer
+model.add(Activation('selu'))
+
+model.add(Dense(300, kernel_initializer='random_normal')) # Third layer
+model.add(Activation('tanh'))
+
+# model.add(Dense(50, kernel_initializer='random_normal')) # Fourth layer
 # model.add(Activation('relu'))
 
 model.add(Dense(10, kernel_initializer='he_normal')) # last layer
@@ -82,12 +88,72 @@ model.compile(optimizer='sgd',
               metrics=['accuracy'])
 
 #Train Model
-history = model.fit(np.array(x_train), np.array(y_train),  validation_data = (np.array(x_val), np.array(y_val)), epochs=10, batch_size=512)
+history = model.fit(np.array(x_train), np.array(y_train),  validation_data = (np.array(x_val), np.array(y_val)), epochs=250, batch_size=128)
 
 
 #Report Results
-#print(history.history)
+val_accuracy = history.history['val_accuracy']
+train_accuracy = history.history['accuracy']
+
+# Plotting Learning curves for Training and Validation Data
+
+plt.plot(range(250),train_accuracy,'r', label = 'Training Accuracy')
+plt.plot(range(250),val_accuracy,'b', label = 'Validation Accuracy')
+plt.legend(loc='lower right')
+plt.xlabel('Number of Epochs')
+plt.ylabel('Training / Validation Accuracy')
+plt.show()
+
+
+
+
+x = model.evaluate(np.array(x_test), np.array(y_test), verbose=1)
+print ("Accuracy of Test Set is: ", str(x[1]))
+
 prediction = model.predict(np.array(x_test))
-# x = model.evaluate(np.array(x_test), np.array(y_test), verbose=1)
-print(np.argmax(prediction[0]))
-print(np.argmax(y_test[0]))
+
+# Detection of 3 Misclassified Images
+count = 0
+misClassifiedImages = []
+for i in range(len(y_test)):
+    if(np.argmax(y_test[i]) != np.argmax(prediction[i])):
+        img = np.array(x_test[i]).reshape(28,28)
+        misClassifiedImages.append(img)
+        count+=1
+    if count==3:
+        break
+
+for image in misClassifiedImages:
+    plt.imshow(image, cmap = 'gray', vmin = 0, vmax = 255)    
+    plt.show()
+
+
+# Constructing Confusion Matrix
+confusionMatrix = []
+for i in range(10):
+    l = []
+    for j in range(10):
+        l.append(0)
+    confusionMatrix.append(l)
+
+for i in range(len(y_test)):
+    true_val = np.argmax(y_test[i])
+    predicted_val = np.argmax(prediction[i])
+    confusionMatrix[true_val][predicted_val]+= 1
+
+# Printing Confusion Matrix
+print("Confusion Matrix is:")
+print("   0 1 2 3 4 5 6 7 8 9")
+print("   -------------------")
+for i in range(10):
+    print(str(i)+"| ",end = '')
+    for j in range(10):
+        print(str(confusionMatrix[i][j]) + " ",end = '')
+    print()
+
+# Saving model
+model.save("best_trained_model")
+
+
+
+
